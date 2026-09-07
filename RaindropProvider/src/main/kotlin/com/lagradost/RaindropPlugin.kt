2603782bbb1ec5dd24e2f8f978018463c8bc138a
samih2000 <samih.mahesar@yahoo.com>
2026-09-07 09:48:57 +0500

    Add RaindropPlugin for Cloudstream integration

---
+++ RaindropProvider/src/main/kotlin/com/lagradost/RaindropPlugin.kt
@@ -0,0 +1,12 @@
+package com.lagradost
+
+import android.content.Context
+import com.lagradost.cloudstream3.plugins.CloudstreamPlugin
+import com.lagradost.cloudstream3.plugins.Plugin
+
+@CloudstreamPlugin
+class RaindropPlugin : Plugin() {
+    override fun load(context: Context) {
+        registerMainAPI(RaindropProvider())
+    }
+}
