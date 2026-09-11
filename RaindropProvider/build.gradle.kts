79abcb4ea6f82ca14fb17f9448f6ff04986e4bc0
samih2000 <samih.mahesar@yahoo.com>
2026-09-11 11:20:45 +0500

    Clean up compile options in build.gradle.kts
    Removed Java and Kotlin compile options from build.gradle.kts

--- RaindropProvider/build.gradle.kts
+++ RaindropProvider/build.gradle.kts
@@ -12,16 +12,6 @@ android {
     defaultConfig {
         minSdk = 21
     }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_11
-        targetCompatibility = JavaVersion.VERSION_11
-    }
-}
-
-tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
-    kotlinOptions {
-        jvmTarget = "11"
-    }
 }
 
 dependencies {
