de454bd93b20335cf9f3c24822708964e9f032b2
samih2000 <samih.mahesar@yahoo.com>
2026-09-14 09:57:40 +0500

    Add build.gradle.kts for TwitterAccounts module

---
+++ TwitterAccounts/build.gradle.kts
@@ -0,0 +1,19 @@
+version = 1
+
+cloudstream {
+    description = "Browse media from specific X/Twitter accounts (requires login cookies)"
+    authors = listOf("YourName")
+    status = 1
+    tvTypes = listOf("Others")
+    iconUrl = "https://abs.twimg.com/favicons/twitter.ico"
+}
+
+android {
+    defaultConfig {
+        minSdk = 21
+    }
+}
+
+dependencies {
+    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+}
